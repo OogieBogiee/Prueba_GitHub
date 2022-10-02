92f3ea2908a8dd2a658a23ceae9c9c702d014ad8
OogieBogiee <jimmycopavargas@outlook.com>
2022-10-01 23:33:13 -0400

    Nuevo

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba en GIT</title>
+</head>
+<body>
+    <h1>Hola en html</h1>
+    <?php
+    $nombre = "prueba";
+    echo "Hola en PHP y esto es una " .$nombre. " para Github";
+    ?>
+<table>
+    <hr>Hola hola</hr>
+    <hr>Otra para para probar</hr>
+    <hr></hr>
+</table>
+</body>
+</html>
